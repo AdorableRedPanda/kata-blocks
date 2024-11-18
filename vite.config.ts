2d03cebdb7ae28022ba153667f6258e20864124a
user <gktin@yandex.ru>
2024-11-18 10:02:02 +0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '',
+  build: {
+    outDir: '../dist',
+  },
+  root: './src/',
+  server: {
+    open: true,
+    port: 8080
+  }
+});
